2b69a8e7c4a5c8811dec7073d2d23dff728b4c8e
V神 <41903645@qq.com>
2015-02-11 00:46:23 +0800

    add a jobs queue that it depends on the cache(redis).

--- server.js
+++ server.js
@@ -3,11 +3,35 @@ var logger = require('koa-logger');
 var serve = require('koa-static');
 var parse = require('co-body');
 var path = require('path');
+var kue = require('kue');
+
+// #### 捕捉全局异常 ####
+process.on('uncaughtException', function(err) {
+  console.error("全局异常捕获，保证程序不会终止: %s", err);
+  console.trace(err.stack);
+});
+
+// #### 注册任务队列处理机制 #####
+var jobs = kue.createQueue({
+  redis: {
+    port: 6379,
+    host: "127.0.0.1",
+    db: 3
+  }
+});
+jobs.watchStuckJobs();
+['online-nodejs'].forEach(function(each) {
+  jobs.process(each, 5, require("./lib/processor/" + each)); // parallel processing 5 jobs
+});
+if(process.env.NODE_ENV != 'production') kue.app.listen(3100);
 
 
+// ### create koa ###
 var app = module.exports = koa();
 
+app.use(serve(path.resolve(__dirname, 'assets')));
 app.use(logger());
+
 
 // ##### custom 404 #####
 app.use(function*(next) {
@@ -15,9 +39,6 @@ app.use(function*(next) {
   if (this.body || !this.idempotent) return;
   this.redirect('/404.html');
 });
-
-app.use(serve(path.resolve(__dirname, 'assets')));
-
 
 // error handler
 app.on('error', function(err) {
