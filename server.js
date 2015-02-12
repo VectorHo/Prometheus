6c22ab7071720cb4bbe15adf993a34b7a7776c5b
V神 <41903645@qq.com>
2015-02-13 00:42:55 +0800

    session support.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var koa = require('koa');
 var logger = require('koa-logger');
 var mount = require('koa-mount');
+var json = require('koa-json');
 var requestId = require('koa-request-id');
+var csrf = require('koa-csrf');
+var session = require('koa-session-redis');
 var serve = require('koa-static');
 var compress = require('koa-compress');
 var conditional = require('koa-conditional-get');
@@ -10,11 +13,11 @@ var parse = require('co-body');
 var route = require('koa-route');
 var kue = require('kue');
 var path = require('path');
-var config = require('./lib/config.js');// 全局配置
+var config = require('./lib/config.js'); // 全局配置
 
 // #### 捕捉全局异常 ####
 process.on('uncaughtException', function(err) {
-  console.error("全局异常捕获，保证程序不会终止: %s", err);
+  console.error("全局异常捕获，保证程序不会终止:\n\t%s", err);
   console.trace(err.stack);
 });
 
@@ -24,25 +27,55 @@ jobs.watchStuckJobs();
 ['online-nodejs'].forEach(function(each) {
   jobs.process(each, 5, require("./lib/processor/" + each)); // parallel processing 5 jobs
 });
-if (process.env.NODE_ENV != 'production') kue.app.listen(config.kue.port);
+if (app.env != 'production') kue.app.listen(config.kue.port);
 
 // ### create koa ###
 var app = module.exports = koa();
+app.name = config.app.name;
+app.proxy = config.app.proxy; // 是否允许前置代理，eg：nginx
+// middleware
 app.use(requestId()); // 为request生成uuid -> this.id(https://github.com/segmentio/koa-request-id)
 app.use(compress()); // Compress
+app.use(json()); // Default to being disabled (useful in ENV = production), but togglable via the query-string parameter
 app.use(logger());
 app.use(conditional()); // etag works together with conditional-get
 app.use(etag());
 // 锦上添花
 app.use(function*(next) {
   yield next;
+  this.set('request-Id', this.id);
   this.set('X-Powered-By', 'Prometheus');
   this.set('Server', 'koa');
   this.set('X-Pretty-Print', true);
 });
-// TODO auth
-
 app.use(serve(path.resolve(__dirname, 'assets')));
+
+// session support
+app.keys = [config.app.key];
+app.use(session({
+  store: {
+    host: process.env.SESSION_PORT_6379_TCP_ADDR || config.redis.url,
+    port: process.env.SESSION_PORT_6379_TCP_PORT || config.redis.port,
+    ttl: config.redis.ttl
+  }
+}));
+// custom 500：下游统一 throw error
+app.use(function*(next) {
+  try {
+    yield next;
+  } catch (err) {
+    console.error('error handle：\n\t%s', err);
+    if (this.path.indexOf('/api') != -1) {
+      this.throw(err);
+    } else {
+      this.redirect('/500.html');
+    }
+  }
+});
+// Authentication 身份验证通过后能在this.session.user
+app.use(require('./lib/router/users').Authentication);
+// persistence logs
+app.use(require('./lib/router/logs').log);
 
 // #### RESTFUL路由：处理业务 ####
 app.use(route.get('/', function*() {
@@ -53,8 +86,8 @@ app.use(route.get(config.app.api_v1, function*() {
     meta: 'TODO meta、url、link ETC.'
   };
 }));
-require('./lib/router/users')(app, route);
-require('./lib/router/logs')(app, route);
+require('./lib/router/users').route(app, route);
+require('./lib/router/logs').route(app, route);
 require('./lib/router/servers')(app, route);
 require('./lib/router/projects')(app, route);
 require('./lib/router/jobs')(app, route);
@@ -63,13 +96,20 @@ require('./lib/router/jobs')(app, route);
 app.use(function*(next) {
   yield next;
   if (this.body || !this.idempotent) return;
-  this.redirect('/404.html');
+  if(this.path.indexOf('/api') != -1) {
+    var err = new Error('Not Found...');
+    err.status = 404;
+    err.url = this.protocol.concat('://', this.host, this.originalUrl);
+    this.throw(404, err);
+  } else {
+    this.redirect('/404.html');
+  }
 });
 
 // ##### error handler #####
-app.on('error', function(err) {
-  if (process.env.NODE_ENV != 'test') {
-    console.log('sent error %s to the cloud', err.message);
+app.on('error', function(err, ctx) {
+  if (process.env.NODE_ENV != 'test') { // output to stderr
+    console.error('sent error %s, %s to the cloud\n', err, ctx);
     console.trace(err.stack);
   }
 });
